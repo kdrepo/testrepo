5dafbfca5b09ab161396f41e1750d58e05313702
username-kd <digital95geek@gmail.com>
2024-10-30 23:26:18 +0530

    changed both files for feature branch

--- hellogithub.py
+++ hellogithub.py
@@ -1,1 +1,21 @@
 print("Hello Github")
+
+
+class laborclass:
+    def __init__(self, iq, age):
+        self.iq = iq
+        self.age = age
+
+    def status(self):
+        if self.age >= 25 or self.iq <=120:
+            print(self.age, "is old and dumb")
+        else: print("You r ok")
+
+        
+
+    
+vivek = laborclass(200, 20)
+vivek.status()
+
+    
+    
--- testfile.py
+++ testfile.py
@@ -1,4 +1,12 @@
+import hellogithub
+
+from hellogithub import laborclass 
+
+
 print("testfile")
 
 for x in range(1,5):
     print(f'The index is {x-1}')
+
+guddu = laborclass(150, 50)
+guddu.status()
